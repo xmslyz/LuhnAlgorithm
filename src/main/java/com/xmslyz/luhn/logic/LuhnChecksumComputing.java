8a13077f74f8048b59560290ab43075abcc285ec
xmslyz <103496747+xmslyz@users.noreply.github.com>
2022-10-19 14:26:21 +0200

    pozbyłem się pól klasowych "ze względu na bezpieczeństwo wielowątkowe" ;} cokolwiek by to miało znaczyć.

--- src/main/java/com/xmslyz/luhn/logic/LuhnChecksumComputing.java
+++ src/main/java/com/xmslyz/luhn/logic/LuhnChecksumComputing.java
@@ -4,47 +4,46 @@ import java.math.BigInteger;
 
 public class LuhnChecksumComputing implements Computable {
 
-    int total, checkedNumeral, digitLength, arrInt;
-    String checkSum;
-    BigInteger number;
-    String[] array;
-
     @Override
     public String compute(Candidate candidate) {
-        number = candidate.getNumber();
-        array = candidate.getDigits();
-        digitLength = candidate.getDigitCount();
-        multiplyWages();
-        countTotal();
-        getChecksum();
-        return checkSum;
+        BigInteger number = candidate.getNumber();
+        String[] array = candidate.getDigits();
+        int digitLength = candidate.getDigitCount();
+
+        String[] wagedArray = multiplyWages(array, digitLength);
+        int total = countTotal(wagedArray);
+        return getChecksum(number, total);
     }
 
-    private void multiplyWages() {
+    private String[] multiplyWages(String[] array, int digitLength) {
         for (int i = digitLength; i > 0; i -= 2) {
-            checkedNumeral = Integer.parseInt(array[i - 1]);
+            int checkedNumeral = Integer.parseInt(array[i - 1]);
             checkedNumeral *= 2;
             if (checkedNumeral % 10 > 0 || checkedNumeral == 10) {
                 array[i - 1] = String.valueOf((checkedNumeral / 10) + (checkedNumeral % 10));
             }
         }
+        return array;
     }
 
-    private void countTotal() {
+    private int countTotal(String[] array) {
+        int total = 0;
         for (String s : array) {
-            arrInt = Integer.parseInt(s);
+            int arrInt = Integer.parseInt(s);
             total += arrInt;
         }
+        return total;
     }
 
-    private void getChecksum(){
+    private String getChecksum(BigInteger number, int total){
         int mod10 = total % 10;
-
+        String checkSum;
         if (mod10 != 0) {
             int codeNumber = 10 - mod10;
             checkSum = number.toString() + codeNumber;
         } else {
             checkSum = number.toString() + 0;
         }
+        return checkSum;
     }
 }
